e2f5df833cc772fc83486378d54fbb6091f98ecf
chutch01 <chutch01@hotmail.com>
2015-03-19 15:16:03 -0400

    added more constants and added scoreboard object

--- COMP2068-ScrollerAssignment/Scripts/constants.ts
+++ COMP2068-ScrollerAssignment/Scripts/constants.ts
@@ -4,4 +4,15 @@
     export var PLAY_STATE: number = 1;
     export var GAME_OVER_STATE: number = 2;
     export var HOW_TO_STATE: number = 3;
+
+    //font constants
+    export var FONT_SIZE: string = "40px";
+    export var FONT_FAMILY: string = "Consolas";
+    export var FONT_COLOUR: string = "#FFFF00";
+    //stage constants
+    export var SCREEN_WIDTH: number = 640;
+    export var SCREEN_HEIGHT: number = 480;
+    export var SCREEN_CENTER_WIDTH: number = SCREEN_WIDTH * 0.5;
+    export var SCREEN_CENTER_HEIGHT: number = SCREEN_HEIGHT * 0.5;
+    export var OCEAN_RESET_HEIGHT: number = 960;
 }
